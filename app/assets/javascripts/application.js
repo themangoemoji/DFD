9d4e8d9a1ed329aeaf8671e9f974a7324b553691
Michael Wright <wrigh517@gmail.com>
2014-12-21 12:23:15 -0500

    Better member images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,17 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+  $('#js-navigation-menu').removeClass("show");
+
+  $('#js-mobile-menu').on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
+
