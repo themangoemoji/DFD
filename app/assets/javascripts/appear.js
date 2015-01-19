66ac06e999164444ebcba7bf9a3e75e24ac906ca
Michael Wright <wrigh517@gmail.com>
2015-01-19 01:39:03 -0500

    Add scroll fading to history paragraphs

--- app/assets/javascripts/appear.js
+++ app/assets/javascripts/appear.js
@@ -1,22 +1,28 @@
+
+$(function(){  // $(document).ready shorthand
+  $('.monster').fadeIn('slow');
+});
+
 $(document).ready(function() {
-  $(".fade-in-element").each(function(element) { element.addClass("js-fade-element-hide"); });
-
-  $(window).scroll(function() {
-    if( $(".fade-in-element").length > 0 ) {
-      var elementTopToPageTop = $(element).offset().top;
-      var windowTopToPageTop = $(window).scrollTop();
-      var windowInnerHeight = window.innerHeight;
-      var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
-      var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
-      var distanceFromBottomToAppear = 300;
-
-      if(elementTopToWindowBottom > distanceFromBottomToAppear) {
-        $(element).addClass('js-fade-element-show');
-      }
-      else if(elementTopToWindowBottom < 0) {
-        $(element).removeClass('js-fade-element-show');
-        $(element).addClass('js-fade-element-hide');
-      }
-    }
-  });
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
 });
