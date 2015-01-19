ca98786660e68e2a81d0cd9e2395d1b28cf9bb6d
Michael Wright <wrigh517@gmail.com>
2015-01-19 01:06:24 -0500

    Add History text

---
+++ app/assets/javascripts/appear.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $(".fade-in-element").each(function(element) { element.addClass("js-fade-element-hide"); });
+
+  $(window).scroll(function() {
+    if( $(".fade-in-element").length > 0 ) {
+      var elementTopToPageTop = $(element).offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var windowInnerHeight = window.innerHeight;
+      var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+      var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+      var distanceFromBottomToAppear = 300;
+
+      if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+        $(element).addClass('js-fade-element-show');
+      }
+      else if(elementTopToWindowBottom < 0) {
+        $(element).removeClass('js-fade-element-show');
+        $(element).addClass('js-fade-element-hide');
+      }
+    }
+  });
+});
